5295ee2e3eccd7d6d20443b980b395c0d02a4d9e
Seamoon Pandey <hackedasocial@gmail.com>
2024-12-29 21:22:02 +0545

    basic setup with router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home"
+import Work from "./pages/Work"
+import Contact from "./pages/Contact"
+import About from "./pages/About"
+
 export default function App() {
-    return <div className="text-center text-red-400 text-3xl">
-        Hello world
-    </div>
+    return <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+
+    </Routes>
 }
