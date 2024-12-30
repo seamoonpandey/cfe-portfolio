72dac51bb12ca4b47f638347b7c656dafed8a137
Seamoon Pandey <hackedasocial@gmail.com>
2024-12-30 21:53:55 +0545

    last commit for monday

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,15 +1,47 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
+import { useLocation } from "react-router";
+
+const navItems = [
+    { name: "Home", route: "/" },
+    { name: "About", route: "/about" },
+    { name: "Work", route: "/work" },
+    { name: "Contact", route: "/contact" },
+];
+
 
 export default function Navbar() {
+
+    const router = useLocation();
+
+    const [location, setLocation] = useState("");
+
+    useEffect(() => {
+        setLocation(router.pathname);
+    }, [router]);
+
+
+
+
     return (
         <nav className="bg-transparent absolute flex justify-evenly items-center w-full">
             <div id="logo" className="text-white font-bold">
                 Logo
             </div>
-            <ul className="flex gap-5 text-white text-[.5rem]">
-                <li>Home</li>
-                <li>About</li>
-                <li>Work</li>
-                <li>Contact</li>
+            <ul className="flex gap-5 text-purple-400 text-[.5rem]">
+                {
+                    navItems.map((item) => {
+                        console.log(location.pathname);
+                        return (
+                            <li
+                                key={item}>
+                                <Link to={item.route}>
+                                    {item.name}
+                                </Link>
+                            </li>
+                        )
+                    })
+                }
             </ul>
         </nav>
     )
