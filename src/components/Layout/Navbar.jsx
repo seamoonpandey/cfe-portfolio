b4a4145ddee9d684565e5cbbfcdecccbff46beb3
Seamoon Pandey <hackedasocial@gmail.com>
2024-12-30 21:35:25 +0545

    creaate the nav layout

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,6 +1,16 @@
 
 export default function Navbar() {
     return (
-        <nav>This is the navbar</nav>
+        <nav className="bg-transparent absolute flex justify-evenly items-center w-full">
+            <div id="logo" className="text-white font-bold">
+                Logo
+            </div>
+            <ul className="flex gap-5 text-white text-[.5rem]">
+                <li>Home</li>
+                <li>About</li>
+                <li>Work</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
     )
 }
