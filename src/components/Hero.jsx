54b9e177b26399e66ef1b8e1792a65446391e88a
Seamoon Pandey <hackedasocial@gmail.com>
2024-12-30 21:16:13 +0545

    add hero section with nice gradient

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+
+export default function Hero() {
+    return (
+        <div className="h-dvh bg-hero-gradient flex justify-center items-center">
+            <div className="bg-gradient-to-t from-blue-200 to-pink-200 bg-clip-text text-transparent text-2xl font-bold">
+                This is my hero section
+            </div>
+        </div>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
+import Hero from "../components/Hero";
 
 export default function Home() {
     return (
-        <div>Home</div>
+        <div>
+            <Hero />
+
+        </div>
     )
 }
